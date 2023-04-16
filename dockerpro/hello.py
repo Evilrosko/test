5e6de4309b81d1d6545f6cb2d61068307ca93703
karolis.bachvalovas <karoliux911@gmail.com>
2023-04-16 16:53:10 +0300

    1sec repeat

--- dockerpro/hello.py
+++ dockerpro/hello.py
@@ -1,4 +1,11 @@
 import os
-with open('new.txt','r') as f:
-    contents = f.read()
-    print(contents)
+import socket
+import time
+starttime = time.time()
+while True:
+        print(socket.gethostname())    
+        with open('new.txt','r') as f:
+            contents = f.read()
+        print(contents)
+        
+        time.sleep(1.0 - ((time.time() - starttime) % 1.0))
